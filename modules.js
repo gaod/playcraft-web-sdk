2c28abbc1ddc205387513c0e4cfef3659f6476c7
KKStream TRC <trc-bot@kkstream.com>
2022-08-19 12:33:14 +0000

    chore(release): 1.15.8

--- modules.js
+++ modules.js
@@ -467,7 +467,7 @@ const getSource = (sourceOptions, {
 function getVersion() {
   try {
     // eslint-disable-next-line no-undef
-    return "1.15.7";
+    return "1.15.8";
   } catch (e) {
     return undefined;
   }
